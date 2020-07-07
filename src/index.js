a01351b0f3561fa2b9b478d3227896f263e64501
karthi07 <itkarthi02@gmail.com>
2020-07-07 13:52:24 +0530

    fix small typo

--- src/index.js
+++ src/index.js
@@ -34,8 +34,7 @@ navElement.innerHTML = `
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-  </div>
-</nav>`;
+  </div>`;
 
 const mainElement = document.createElement('main');
 mainElement.classList = 'container';
